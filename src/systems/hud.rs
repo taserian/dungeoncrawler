4c5585edfbd31cc643ccf9a5b479333a2f36340b
Antonio Rodriguez <antonio.rodriguez@virtuescript.com>
2024-04-03 13:37:50 -0400

    Minor change to the hud.

--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -13,7 +13,7 @@ pub fn hud(ecs: &SubWorld) {
 
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(2);
-    draw_batch.print_centered(1, "Explore the Dungeon. Cursor keys to move.");
+    draw_batch.print_centered(1, "Explore the Dungeon. Cursor keys to move. 'G' to get items.");
     draw_batch.bar_horizontal(
         Point::zero(),
         SCREEN_WIDTH * 2,
